a5fc5ba525ee880e227b437f15675a909a3dafa0
Ruhi Jame <ruhi.jame@gmail.com>
2025-06-18 12:44:42 -0400

    Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by Ruhi Jame on 6/17/25.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
